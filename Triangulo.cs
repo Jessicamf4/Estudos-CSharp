a0e87c5d4fe5d9dd446c73ae898940b7ff3f76c4
Jéssica Marques <102704266+Jessicamf4@users.noreply.github.com>
2023-03-09 19:44:17 +0000

    Triangulo OPP

---
+++ Triangulo.cs
@@ -0,0 +1,6 @@
+
+class Triangulo{
+  public double A;
+  public double B; 
+  public double C;
+}
--- main.cs
+++ main.cs
@@ -1,19 +1,39 @@
 using System;
+using System.Globalization;
 
 class Program{
+	static void Main (string []args){
 
-static void Main(){
+  Triangulo x, y;
+  y = new Triangulo();
+  x = new Triangulo();
 
-  int nota;
-	string resultado="Reprovado";
+  double areax, px;
+  double areay, py;
 
-	Console.WriteLine("Digite sua nota");
-	nota = int.Parse(Console.ReadLine());
+  Console.WriteLine("Digite os três lados de um triângulo:");
 
-	if(nota >= 6){
-	resultado = "Aprovado";
-		}
+  x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+  x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+  x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-	Console.WriteLine("Resultado: {0}", resultado);
+  Console.WriteLine("Digite os três lados de um triângulo:");
+
+  y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+  y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+  y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+  px = (x.A + x.B + x.C) / 2;
+  areax = Math.Sqrt( px * (px - x.A) * (px - x.B) * (px - x.C));
+
+  py = (y.A + y.B + y.C) / 2;
+  areay = Math.Sqrt( py * (py - y.A) * (py - y.B) * (py - y.C));
+
+  if(areax > areay){
+    Console.WriteLine("Maior área: X");
+  }else{
+    Console.WriteLine("Maior área: Y");
   }
-  }
+  
+}
+}
