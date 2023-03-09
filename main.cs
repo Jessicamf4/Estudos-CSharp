c7ae7b13cc0f329370e2f3e057a055e04525eb5c
Jéssica Marques <102704266+Jessicamf4@users.noreply.github.com>
2023-03-09 22:27:56 +0000

    Incluindo métodos

--- main.cs
+++ main.cs
@@ -8,26 +8,20 @@ class Program{
   y = new Triangulo();
   x = new Triangulo();
 
-  double areax, px;
-  double areay, py;
-
-  Console.WriteLine("Digite os três lados de um triângulo:");
+  Console.WriteLine("Digite os três lados de um triângulo X:");
 
   x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
   x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
   x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-  Console.WriteLine("Digite os três lados de um triângulo:");
+  Console.WriteLine("Digite os três lados de um triângulo Y:");
 
   y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
   y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
   y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-  px = (x.A + x.B + x.C) / 2;
-  areax = Math.Sqrt( px * (px - x.A) * (px - x.B) * (px - x.C));
-
-  py = (y.A + y.B + y.C) / 2;
-  areay = Math.Sqrt( py * (py - y.A) * (py - y.B) * (py - y.C));
+  double areax = x.Area();
+  double areay = y.Area();
 
   if(areax > areay){
     Console.WriteLine("Maior área: X");
@@ -35,5 +29,4 @@ class Program{
     Console.WriteLine("Maior área: Y");
   }
   
-}
-}
+}}
